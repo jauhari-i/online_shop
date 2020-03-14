97d3ebc69b6dd6dbdd561b3859d3a1941b81c131
Irfan Jauhari <kangrykong123@gmail.com>
2020-03-14 16:54:03 +0700

    Add badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # online_shop
+
+[![DeepScan grade](https://deepscan.io/api/teams/8079/projects/10227/branches/138490/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=8079&pid=10227&bid=138490)
--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const log = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = 5500;
@@ -11,6 +14,13 @@ if (db) {
 } else {
   console.log("Database Disconnected");
 }
+
+app.use(log("common"));
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/routes"));
 
